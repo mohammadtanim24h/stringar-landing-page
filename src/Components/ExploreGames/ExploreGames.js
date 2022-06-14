49701c3ecf205146648a47dafac49c88e7a4d656
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-06-14 19:20:38 +0600

    explore section badge and avatar added

--- src/Components/ExploreGames/ExploreGames.js
+++ src/Components/ExploreGames/ExploreGames.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ExploreGames.css";
+import notificationFriendsOnly from "../../assets/images/friends.jpg";
 
 const ExploreGames = () => {
     return (
@@ -11,7 +12,14 @@ const ExploreGames = () => {
                     </h2>
                     <div class="avatar absolute bottom-0 left-0 ml-[10px] mb-[10px]">
                         <div class="w-20 rounded-full">
-                            <img src="https://api.lorem.space/image/face?hash=92310" />
+                            <img
+                                className="w-20 object-cover"
+                                src={notificationFriendsOnly}
+                                alt=""
+                            />
+                            <span class="badge bg-red-500 border-0 absolute top-0 left-[65%]">
+                                New
+                            </span>
                         </div>
                     </div>
                 </div>
@@ -21,7 +29,10 @@ const ExploreGames = () => {
                     </h2>
                     <div class="avatar absolute bottom-0 left-0 ml-[10px] mb-[10px]">
                         <div class="w-20 rounded-full">
-                            <img src="https://api.lorem.space/image/face?hash=92310" />
+                            <img
+                                src="https://api.lorem.space/image/face?hash=92310"
+                                alt=""
+                            />
                         </div>
                     </div>
                     <button className="btn btn-primary absolute bottom-0 left-3/4 mb-[10px]">
